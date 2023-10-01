406a1db6f08d7efcec5713a7f6ec086b3ece2829
SunnyGit <fang_sunsf@yahoo.com>
2023-10-01 13:05:08 -0400

    Add all empty components and config the route module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,20 @@ import { AppComponent } from './app.component';
 import {HttpClientModule, } from '@angular/common/http'
 import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api'
 import { InMemoryDataService } from './in-memory-data.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroSearchComponent,
+    HeroesComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
