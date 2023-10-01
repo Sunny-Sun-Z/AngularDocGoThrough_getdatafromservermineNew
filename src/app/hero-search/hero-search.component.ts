0f421054df0ad176944279aa4c932b0f7e54885e
SunnyGit <fang_sunsf@yahoo.com>
2023-10-01 19:05:38 -0400

    hero search using Subject

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from '../hero.service';
+import { Observable, Subject } from 'rxjs';
+import { debounce, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-hero-search',
   templateUrl: './hero-search.component.html',
   styleUrls: ['./hero-search.component.css']
 })
-export class HeroSearchComponent {
+export class HeroSearchComponent implements OnInit{
+  // hero: Hero | undefined;
+  // heroes$!: Observable<Hero[]>;
+  // private searchItems = new Subject<string>();
+
+   constructor(private route: ActivatedRoute, private service: HeroService){}
+
+  // ngOnInit(): void {
+  //   this.heroes$ = this.searchItems.pipe(
+  //     debounceTime(300),
+  //     distinctUntilChanged(),
+     
+  //     switchMap((term:string)=>this.service.searchHeroes(term))
+      
+  //   );
+  // }
+  // search(item: string) {
+  //   this.searchItems.next(item);
+  // }
+
+  heroes$!: Observable<Hero[]>;
+  private searchItem = new Subject<string>();
+
+  ngOnInit(): void {
+    this.heroes$ = this.searchItem.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((item:string)=> this.service.searchHeroes(item))
+    )
+  }
+
+  search(item: string){
+    this.searchItem.next(item);
+  }
 
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
+ // hero: Hero | undefined;
+  heroes: Hero[]=[];
+
+  constructor(private service: HeroService){}
+
+  ngOnInit(): void {
+      this.getHeroes();
+  }
+  getHeroes() {
+     this.service.getHeroes().subscribe(
+      hs=> this.heroes = hs
+    );
+  }
+  add(name: string){
+    if(!name) return;
+
+    this.service.addHero({name} as Hero).subscribe(
+      h=> this.heroes.push(h)
+    );
+  }
+
+  delete(hero:Hero){
+    this.heroes = this.heroes.filter(h=>h!== hero);
+    this.service.deleteHero(hero.id).subscribe();
+  }
 
 }
